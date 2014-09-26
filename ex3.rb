3bd396b207b9588912c1037980fce4a4f093a33b
Elizabeth Norton <enorton10@gmail.com>
2014-09-25 21:51:02 -0400

    Corrected Exercise 3

--- ex3.rb
+++ ex3.rb
@@ -4,6 +4,10 @@ puts "Roosters #{100 - 25 * 3 % 4}"
 
 puts "Now I will count the eggs:"
 
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
 puts 3 + 2 < 5 - 7
 
 puts "What is 3 + 2? #{3 + 2}"
