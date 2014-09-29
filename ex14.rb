cb6048eb57d6207df5b0e5163dfce2e95601f69b
Elizabeth Norton <enorton10@gmail.com>
2014-09-29 15:50:44 -0400

    Corrected 14

--- ex14.rb
+++ ex14.rb
@@ -4,10 +4,13 @@ prompt = '>'
 puts "Hi #{user_name}."
 puts "I'd like to ask you a few questions."
 puts "Do you like me #{user_name}?", prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{username}?", prompt
 lives = $stdin.gets.chomp
 
 puts "What kind of computer do you have?", prompt
-computer = $stdin.get.chomp
+computer = $stdin.gets.chomp
 
 puts """
 Alright, so you said #{likes} about liking me.
