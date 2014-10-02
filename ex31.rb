0d49fba2d8b2a1b07b4947187d2deaee8830f43d
Elizabeth Norton <enorton10@gmail.com>
2014-10-02 17:44:24 -0400

    Corrected 31

--- ex31.rb
+++ ex31.rb
@@ -1,7 +1,7 @@
 puts "You enter a dark room with two doors. Do you go through door #1 or door #2?"
 
-">"
-= $stdin.gets.chomp
+print ">"
+door = $stdin.gets.chomp
 
 if door == "1"
 puts "There's a giant bear here eating a cheesecake. WHat do you do?"
