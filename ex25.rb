646892df8fc0e1339ece2b52da392fb207c180d6
Elizabeth Norton <enorton10@gmail.com>
2014-10-02 00:42:20 -0400

    Finished 24

---
+++ ex25.rb
@@ -0,0 +1,19 @@
+module Ex25
+
+	# This function will brea up words for us.
+	def Ex25.break_words(stuff)
+		words = stuff.split(' ')
+		return wordsend
+	end
+
+	# Sorts the words
+	def Ex25.sort_words(words)
+		return words.sort_words
+	end
+
+	# Print the first word after shifting it off
+	def Ex25.print_first_word(words)
+		word = words.shift
+		puts word
+	end
+	
